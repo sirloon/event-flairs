6aa7b01a0c58b2a9b74ce46c13ef9f53876eb1ea
sebastien.lelong <sebastien.lelong@gmail.com>
2013-08-07 00:44:47 +0200

    custom flairs section in website, with export feature

---
+++ static/js/sets.js
@@ -0,0 +1,13 @@
+
+var sets = [
+	{'name' : 'Original flairs',
+	 'author' : 'Google',
+	 'description' : 'Flairs from original event flairs lab',
+	 'def' : {"https://www.google.com/googlecalendar/icons/":["cake.png","fork.png","martini.png","gift.png","heart.png","happy.png","sad.png","angry.png","home.png","work.png","car.png","plane.png","bike.png","bill.png","doctor.png","skull.png","sport_football.png","sport_baseball.png","sport_basketball.png","sport_hockey.png","sport_golf.png","sport_tennis.png","sport_soccer.png","sport_rugby.png","4.png","3.png","2.png","1.png","5.png","6.png","tv.png","book.png","9.png","10.png","11.png","12.png","8.png","7.png","phone.png","vacation.png"]}
+	},
+	{'name' : 'More to come!',
+	 'author' : 'You',
+	 'description' : '<a href="https://groups.google.com/forum/#!forum/event-flairs">Join</a> the group and share your favorite flairs set',
+	}
+
+];
