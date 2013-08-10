abe249026b0818c5eebe2245935a289800f00ee3
sebastien.lelong <sebastien.lelong@gmail.com>
2013-08-10 11:21:10 +0200

    fatcow flairs set

--- static/js/sets.js
+++ static/js/sets.js
@@ -5,15 +5,15 @@ var sets = [
 	 'description' : 'Flairs from original event flairs lab',
 	 'def' : {"https://www.google.com/googlecalendar/icons/":["cake.png","fork.png","martini.png","gift.png","heart.png","happy.png","sad.png","angry.png","home.png","work.png","car.png","plane.png","bike.png","bill.png","doctor.png","skull.png","sport_football.png","sport_baseball.png","sport_basketball.png","sport_hockey.png","sport_golf.png","sport_tennis.png","sport_soccer.png","sport_rugby.png","4.png","3.png","2.png","1.png","5.png","6.png","tv.png","book.png","9.png","10.png","11.png","12.png","8.png","7.png","phone.png","vacation.png"]}
 	},
+	{'name' : 'Fatcow flairs',
+	 'author' : 'Seb',
+	 'description' : 'Closed to original, but using <a href="http://www.fatcow.com/free-icons">Fatcow</a> icons',
+	 'def' : {"https://event-flairs.googlecode.com/git/static/images/flairs/fatcow/":["piece_of_cake.png","cutleries.png","glass_narrow.png","gift_add.png","heart.png","emotion_happy.png","emotion_sad.png","emotion_angry.png","house.png","handbag.png","car.png","plane.png","camera.png","money_bag.png","hospital.png","scull.png","sport_football.png","sport_tennis.png","sport_basketball.png","sport_shuttlecock.png","sport_golf.png","sport_8ball.png","sport_soccer.png","user_ballplayer.png","award_star_bronze_2.png","award_star_silver_2.png","award_star_silver_1.png","award_star_gold_1.png","award_star_gold_3.png","award_star_silver_2.png","television.png","book_open.png","tick_circle_frame.png","exclamation.png","control_fastforward_blue.png","tick_red.png","information.png","question.png","phone_vintage.png","yin_yang.png"]}
+	},
 	{'name' : 'Minimalistic B&W flairs',
 	 'author' : 'Seb',
 	 'description' : 'Minimalistic icon set, using <a href="http://somerandomdude.com/work/iconic/">Iconic</a> collection',
 	 'def' : {"https://event-flairs.googlecode.com/git/static/images/flairs/iconic/black/":["stop_16x16.png","cd_16x16.png","brush_16x16.png","pen_alt2_16x16.png","arrow_left_16x16.png","arrow_right_16x16.png","arrow_up_16x16.png","arrow_down_16x16.png","bars_16x16.png","at_16x16.png","bolt_16x16.png","camera_16x16.png","check_alt_16x16.png","clock_16x16.png","cog_16x16.png","folder_fill_16x16.png","home_16x16.png","image_16x16.png","key_fill_16x16.png","key_stroke_16x16.png","magnifying_glass_16x16.png","move_alt2_16x16.png","move_horizontal_alt2_16x16.png","pin_16x16.png","play_alt_16x16.png","rss_16x16.png","star_16x16.png","aperture_16x16.png","tag_fill_16x16.png","tag_stroke_16x16.png","umbrella_16x16.png","x_alt_16x16.png"]}
-	},
-	{'name' : 'Minimalistic Magenta flairs',
-	 'author' : 'Seb',
-	 'description' : 'Minimalistic icon set, using <a href="http://somerandomdude.com/work/iconic/">Iconic</a> collection',
-	 'def' : {"https://event-flairs.googlecode.com/git/static/images/flairs/iconic/magenta/":["stop_16x16.png","cd_16x16.png","brush_16x16.png","pen_alt2_16x16.png","arrow_left_16x16.png","arrow_right_16x16.png","arrow_up_16x16.png","arrow_down_16x16.png","bars_16x16.png","at_16x16.png","bolt_16x16.png","camera_16x16.png","check_alt_16x16.png","clock_16x16.png","cog_16x16.png","folder_fill_16x16.png","home_16x16.png","image_16x16.png","key_fill_16x16.png","key_stroke_16x16.png","magnifying_glass_16x16.png","move_alt2_16x16.png","move_horizontal_alt2_16x16.png","pin_16x16.png","play_alt_16x16.png","rss_16x16.png","star_16x16.png","aperture_16x16.png","tag_fill_16x16.png","tag_stroke_16x16.png","umbrella_16x16.png","x_alt_16x16.png"]}
 	},
 	{'name' : 'More to come!',
 	 'author' : 'You',
