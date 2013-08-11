d3ea5923506d3ac7cbce515a056198bd556c3f98
sebastien.lelong <sebastien.lelong@gmail.com>
2013-08-11 10:49:59 +0200

    glyphicons reposistory

--- static/images/flairs/master.js
+++ static/images/flairs/master.js
@@ -3,6 +3,10 @@ var MASTER = [
  "url"  : "http://somerandomdude.com/work/iconic/",
  "def"  : "https://event-flairs.googlecode.com/git/static/images/flairs/iconic/repository.js"
 },
+{"name" : "Glyphicons",
+ "url"  : "http://glyphicons.com/",
+ "def"  : "https://event-flairs.googlecode.com/git/static/images/flairs/glyphicons/repository.js"
+},
 {"name" : "FatCow",
  "url"  : "http://www.fatcow.com/free-icons",
  "def"  : "https://event-flairs.googlecode.com/git/static/images/flairs/fatcow/repository.js"
