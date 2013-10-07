e78dd20c31d56ff896be50e0635f77c04726221a
sebastien.lelong <sebastien.lelong@gmail.com>
2013-10-07 20:35:16 +0200

    added Amanda's custom flairs

--- static/js/sets.js
+++ static/js/sets.js
@@ -20,6 +20,11 @@ var sets = [
 	 'description' : 'Minimalistic icon set, using <a href="http://somerandomdude.com/work/iconic/">Iconic</a> collection',
 	 'def' : {"https://event-flairs.googlecode.com/git/static/images/flairs/iconic/":["black_stop.png","blue_stop.png","brown_dark_stop.png","brown_light_stop.png","cyan_stop.png","gray_dark_stop.png","gray_light_stop.png","green_stop.png","magenta_stop.png","orange_stop.png","red_stop.png","tan_stop.png","yellow_stop.png","black_cd.png","blue_cd.png","brown_dark_cd.png","brown_light_cd.png","cyan_cd.png","gray_dark_cd.png","gray_light_cd.png","green_cd.png","magenta_cd.png","orange_cd.png","red_cd.png","tan_cd.png","yellow_cd.png","black_layers_alt.png","blue_layers_alt.png","brown_dark_layers_alt.png","brown_light_layers_alt.png","cyan_layers_alt.png","gray_dark_layers_alt.png","gray_light_layers_alt.png","green_layers_alt.png","magenta_layers_alt.png","orange_layers_alt.png","red_layers_alt.png","tan_layers_alt.png","yellow_layers_alt.png"]}
 	},
+	{'name' : 'Amanda flairs',
+	 'author' : 'Amanda',
+	 'description' : 'Amanda flairs, based on <a href="http://www.fatcow.com/free-icons">Fatcow</a> collection',
+	 'def' : {"https://goo.gl/":["rKHNLj","Wv9zi8","87SSjE","hMWNlU","KuNg7s","M2fTuK","dpTDr3","R7ze4S","nSgtYG","VM9m2A","Cr2rGK","W6HoMN","Bo2y4b","25sRfy","q0dvOZ","444C8C","PnqzC9","3gGseP","ZpNsgW","0I6uwd","568Lqk","RTgNzY","aVXvKh","qsAo7f","zXVk9D","MLa4Ct","nXEKGk","ZC5kPt","4OQDqZ","sG92an","2EQ8d2","9XZCoL","uUWHkY","fIE716","7cS20z","Oo93JP","03n0we","4NCRGF","HPHQXu","RKVvvn","kFGuro","8pcqsH","2FV4DB","ATClCU","Gfq1C3","O6aobM","R3YhPq","ymzgOI","UsjE4X","vA99pV","y8Y2el","OEEVLm","9rqBPS","WdyGBV","tSJKao","iscRZK","5U8jF4","QtyVFh","lB5S4I","gWNKD5","X5FlZR","1DP4yC","bcb52J","hOsPn7","42bg49","z1tSN0","XWWSMI","mh5kP2","1hfsVK","j6FyUw","TrAQrl","QeTGbm","sSDydk","FTsptv","HFjctb","5D9yFR","yA5GSN","hGvWEL","NGM6jl","Bmvmco","ofNkFC","3DlGmX","zpg3Px","oM2VLh","2Tyl3W","rdPL1S","wqFlCB","dU0Pfe","3sCYUP","mwWueR","MUZiom","YkWV6U","YXYh4H","vpqa6A","PueEjT","lwwKMg","TmKO0i","KLWKJ4","4WsqnK","oxBe8t","Olcgih","qmLYih","0P7qHE","noX2jO"]}
+	},
 	{'name' : 'More to come!',
 	 'author' : 'You',
 	 'description' : '<a href="https://groups.google.com/forum/#!forum/event-flairs">Join</a> the group and share your favorite flairs set',
