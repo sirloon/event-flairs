b2b6d295e4a755e7b51a0af7b4aa151b776eb77b
sebastien.lelong <sebastien.lelong@gmail.com>
2013-10-17 23:31:24 +0200

    issue 11: delete icon then back to normal mode

--- shorturl.py
+++ shorturl.py
@@ -6,62 +6,38 @@ from google.appengine.ext import db
 from google.appengine.api import mail
 
 import os, logging
-import StringIO
-import numpy
-import omr
-import json
+import json, urllib, urllib2
 import traceback
 
 
 # Web app
+API_KEY = 'AIzaSyDkrW_qf_sLGzhFrHPjzDKZ-kXsS9SC22A'
 
-class MainPage(webapp.RequestHandler):
+class ShortUrlPage(webapp.RequestHandler):
     def get(self):
-
-        template_values = {}
-
-        path = os.path.join(os.path.dirname(__file__), 'index.html')
-        self.response.out.write(template.render(path, template_values))
-
-    def send_email(self,subject,body,pix,omrpix):
-        mail.send_mail(sender="sebastien.lelong@gmail.com",
-                       to="shootsheetapps@gmail.com",
-                       subject=subject,
-                       body=body,
-                       attachments=[("pix.jpg",pix),("omr.jpg",omrpix)])
-
-    def post(self):
-        #logging.info("keys: %s" % self.request)
-        pix = self.request.get('Filedata')
-        #logging.error("pix(%s): %s" % (len(pix),pix))
-        buf = StringIO.StringIO(pix)
-        try:
-            if self.request.get('pre'):
-                # search best angle
-                angle = omr.preomr(buf)
-                omrpix,res = omr.omr(buf,angle)
-            else:
-                omrpix,res = omr.omr(buf,0)
-            outb = StringIO.StringIO()
-            omrpix.save(outb,format="JPEG")
-            outb.seek(0)
-            if res['errmsg']:
-                self.send_email("Problem pix from OMR","%s" % res,pix,"")
-        except Exception,e:
-            self.send_email("Error pix from OMR","%s\n%s" % (e,traceback.format_exc()),pix,"")
-            raise
-        self.response.headers['Content-Type'] = 'application/json'
-        json_res = json.dumps(res)
-        if self.request.get('debug'):
-            self.send_email("OK pix from OMR","%s" % res,pix,outb.buf)
+        longurl = self.request.get('longurl')
+        service = self.request.get('service')
+        results = {'short' : None}
+        # bit.ly, in the future ?
+        if service == 'goo.gl':
+            post_url = 'https://www.googleapis.com/urlshortener/v1/url?key=%s' % API_KEY
+            postdata = {'longUrl':longurl}
+            headers = {'Content-Type':'application/json'}
+            req = urllib2.Request(
+                post_url,
+                json.dumps(postdata),
+                headers
+            )
+            ret = urllib2.urlopen(req).read()
+            results['short'] = json.loads(ret)['id'].replace("http://","https://")
+        json_res = json.dumps(results)
+        self.response.headers['Access-Control-Allow-Origin'] = '*'
         self.response.out.write(json_res)
-
-
 
 
 application = webapp.WSGIApplication(
                                      [
-                                        ('/', MainPage),
+                                        ('/shorturl', ShortUrlPage),
                                      ],
                                      debug=True)
 
