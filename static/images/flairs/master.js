f063de827a8eca71283d8d45b8ef6489f48d96c4
sebastien.lelong <sebastien.lelong@gmail.com>
2014-05-29 09:07:20 +0200

    static loaded from appengine

--- static/images/flairs/master.js
+++ static/images/flairs/master.js
@@ -1,14 +1,14 @@
 var MASTER = [
 {"name" : "Iconic",
  "url"  : "http://somerandomdude.com/work/iconic/",
- "def"  : "https://event-flairs.googlecode.com/git/static/images/flairs/iconic/repository.js"
+ "def"  : "https://eventflairs.appspot.com/static/images/flairs/iconic/repository.js"
 },
 {"name" : "Glyphicons",
  "url"  : "http://glyphicons.com/",
- "def"  : "https://event-flairs.googlecode.com/git/static/images/flairs/glyphicons/repository.js"
+ "def"  : "https://eventflairs.appspot.com/static/images/flairs/glyphicons/repository.js"
 },
 {"name" : "FatCow",
  "url"  : "http://www.fatcow.com/free-icons",
- "def"  : "https://event-flairs.googlecode.com/git/static/images/flairs/fatcow/repository.js"
+ "def"  : "https://eventflairs.appspot.com/static/images/flairs/fatcow/repository.js"
 }
 ]
