01254271df339b0db4d605c61b0df6cce6a943b9
sebastien.lelong <sebastien.lelong@gmail.com>
2015-06-06 10:40:49 +0200

    fix js mime-type

---
+++ static/js/master.js
@@ -0,0 +1,14 @@
+var MASTER = [
+{"name" : "Iconic",
+ "url"  : "http://somerandomdude.com/work/iconic/",
+ "def"  : "js/iconic_repository.js"
+},
+{"name" : "Glyphicons",
+ "url"  : "http://glyphicons.com/",
+ "def"  : "js/glyphicons_repository.js"
+},
+{"name" : "FatCow",
+ "url"  : "http://www.fatcow.com/free-icons",
+ "def"  : "js/fatcow_repository.js"
+}
+]
